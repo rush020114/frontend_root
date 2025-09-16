5b562314b84f2a6811a5ab8f02d689f92b7ebf1c
KIMDOYOUNG <januarykdy@gmail.com>
2025-09-16 14:55:17 +0900

    UserControl 및 CSS 작업 완료하였습니다. 또한 Aoo파일에서 라우트 한 줄 추가하여, 그래프가 잘 표시되는지 확인용도로 입력하였습니다. 따라서 추후 App 충돌이슈에 관해서는 팀원이 모여 충돌부분을 제거하면 해결이 가능하오니, 그때 수정부탁드립니다.  팀장님께도 보고드렸습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import MainLayout from './layout/main/MainLayout'
 import Home from './pages/main/Home'
+import UserControl from './pages/user/UserControl'
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Routes>
         {/* 메인 페이지 */}
         <Route path='/' element={<MainLayout />}>
+          {/* 유저 관리 페이지 */}
+          <Route path='bar-chart' element={<UserControl />}/>
           {/* 첫 화면 */}
           <Route path='' element={<Home />} />
         </Route>
