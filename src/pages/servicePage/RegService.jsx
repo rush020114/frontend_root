49171429204418ad3dcbefe2ddc0f0be592c3bf2
rush020114 <rush020114@gmail.com>
2025-09-16 14:51:09 +0900

    날씨 위젯 디자인

--- src/pages/servicePage/RegService.jsx
+++ src/pages/servicePage/RegService.jsx
@@ -4,6 +4,7 @@ import Input from '../../common/Input'
 import Button from '../../common/Button'
 import Textarea from '../../common/Textarea'
 import { useDaumPostcodePopup } from 'react-daum-postcode'
+import WeatherWidget from '../../component/WeatherWidget'
 
 const RegService = () => {
 
