a69ae0132321f5785154961a902ee5eee80a7905
rush020114 <rush020114@gmail.com>
2025-09-22 22:11:15 +0900

    마이페이지 구현 완료

---
+++ src/pages/user/TempHumDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './TempHumDetail.module.css'
+
+const TempHumDetail = () => {
+  return (
+    <div>TempHmdDetail</div>
+  )
+}
+
+export default TempHumDetail
