1b4c7b2c5a2e0613063b606120f027fa431cdcba
rush020114 <rush020114@gmail.com>
2025-10-10 17:36:16 +0900

    오늘의 이용자 수 조회 완료

--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -17,6 +17,19 @@ const Home = () => {
   // 최신 5개만 보이게 할 함수
   const fiveNoticeList = noticeList.slice(0, 5);
 
+
+
+
+
+
+
+  
+
+
+
+
+
+
   // 특정 이미지만 보이게 하는 함수
   const setShow = (index, value) => {
     setIsShow(prev => {
